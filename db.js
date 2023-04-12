9ed008e3dc1ec2e60814ac9d427c41ec9f92012b
Ahmed Olawale <59466195+ayobamy@users.noreply.github.com>
2023-04-12 12:26:37 +0000

    [Update]: reractoring

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = mongoose.connection;
+    db.on('error', () => {
+        console.log('Could not connect to database');
+    });
+    db.once('open', () => {
+        console.log('Connected to database');
+    });
+}
+
+module.exports = dbConnect;
